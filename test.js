5063fceb1f73fbaeeca731cb387df965b93119cf
Prateek Madaan <prince.cenation@gmail.com>
2019-10-11 01:13:27 +0530

    added the very first code for the viewers

---
+++ test.js
@@ -0,0 +1,10 @@
+// Install Node.js before running this script using node command
+
+// For running the script just go to your editor console and type node test.js
+const bookCount = 14;
+
+if(bookCount > 15) {
+  console.log('You are a great reader of books... You admire reading a lot');
+}else {
+  console.log('You still need to read more and more to get that mark');
+}
